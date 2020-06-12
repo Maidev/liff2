3bc071e4dc80008bdc29107937930db71ebba053
Maidev <kathawut@gmail.com>
2020-06-12 11:08:12 +0700

    Add files via upload

--- liff-starter.js
+++ liff-starter.js
@@ -1,9 +1,9 @@
 window.onload = function() {
     const useNodeJS = false;   // if you are not using a node server, set this value to false
-    const defaultLiffId = "";   // change the default LIFF value if you are not using a node server
+    const defaultLiffId = "1654334164-9eA7DR2Y";   // change the default LIFF value if you are not using a node server
 
     // DO NOT CHANGE THIS
-    let myLiffId = "1654334164-9eA7DR2Y";
+    let myLiffId = "";
 
     // if node is used, fetch the environment variable and pass it to the LIFF method
     // otherwise, pass defaultLiffId
